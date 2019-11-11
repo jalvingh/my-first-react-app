c43f82bc5854e265670e7f7b1901e7d27b6379c8
kirstenjalvingh <kirsten.jalvingh@gmail.com>
2019-11-11 13:29:10 +0100

    tryout netlify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & Redux 
+          Learn React & Redux Hi there
         </a>
       </header>
     </div>
